205819f37d7bad9b377a8e11cf9594b3f28c7c23
Jason Gerard <jasongerard321@gmail.com>
2021-02-06 21:21:40 -0500

    #50 updated model

--- app/models/user.py
+++ app/models/user.py
@@ -6,8 +6,8 @@ class User(db.Model):
     __tablename__ = 'user'
 
     user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    full_name = db.Column(db.String(255), nullable=False)
+    full_name = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime)

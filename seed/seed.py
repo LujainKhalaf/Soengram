8bc5d453f90db8954f843d8c93f017a82547bffb
Jason Gerard <jasongerard321@gmail.com>
2021-02-06 21:18:52 -0500

    #50 added regex for email and updated sizes for schema

--- seed/seed.py
+++ seed/seed.py
@@ -1,11 +1,14 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 conn = psycopg2.connect(
-    database="soengram",
-    user="postgres",
-    password="admin",
-    host="localhost",
-    port="5432"
+    database=os.getenv('DB_DATABASE'),
+    user=os.getenv('DB_USERNAME'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT')
 )
 
 cur = conn.cursor()
@@ -13,13 +16,12 @@ cur = conn.cursor()
 cur.execute('''
             CREATE TABLE "user" (
                 user_id SERIAL PRIMARY KEY,
-                email VARCHAR(255) UNIQUE NOT NULL,
-                username VARCHAR(255) UNIQUE NOT NULL,
-                full_name VARCHAR(255) NOT NULL,
+                email VARCHAR(50) UNIQUE NOT NULL,
+                username VARCHAR(20) UNIQUE NOT NULL,
+                full_name VARCHAR(50) NOT NULL,
                 password VARCHAR(255) NOT NULL,
                 created_at TIMESTAMP
             );
             ''')
-
 conn.commit()
 conn.close()

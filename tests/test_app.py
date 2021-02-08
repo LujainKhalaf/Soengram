d09b82370a4473fa0a7dca77e17490fd6654614d
Jason Gerard <jasongerard321@gmail.com>
2021-02-07 21:48:26 -0500

    #51 added endpoint to login and create a session for the user

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,6 @@ from flask import Flask
 from app.app import create_app
 
 
-def test_should_return_flask_app():
+def test_should_return_flask_app() -> None:
     app = create_app()
     assert type(app) is Flask

9f97f1bae895c6bfff3aea7ee0640866bee83b29
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2021-02-08 21:20:47 -0500

    #75 Create base html templates
    - Added the following templates
        - Base
        - Header
        - Navigation
        - Footer
    - Implemented Bootstrap 5.0

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,4 +6,4 @@ index_routes = Blueprint('index_routes', __name__)
 @index_routes.route('/', methods=['GET'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html', message='Hello World!')
+        return render_template('base.html', message='Hello World!')

faeab1824d36688015ae30b3ef676e17c8d144db
Jason Gerard <jasongerard321@gmail.com>
2021-02-08 20:58:36 -0500

    #50 added get route and render template for post redirect pattern (#73)
    * #50 added get route and render template for post redirect pattern
    
    * #50 add get req type in method decorator

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-from flask import request, Blueprint
+from flask import request, Blueprint, render_template
 from app.models.user import User
 from app.services import auth_service
 from app.utils.validation import is_email_valid
@@ -8,18 +8,21 @@ from app.utils.session_decorators import session_required
 auth_routes = Blueprint('auth_routes', __name__)
 
 
-@auth_routes.route('/signup', methods=['POST'])
+@auth_routes.route('/signup', methods=['POST', 'GET'])
 def sign_up() -> Tuple[str, int]:
     if request.method == 'POST':
         full_name, username, email, password = request.form.values()
 
         if not is_email_valid(email):
-            return '', 400
+            return render_template('signup.html', error='Invalid email address')
 
         user = User(full_name=full_name, username=username, email=email)
         auth_service.sign_up(user, password)
 
-        return '', 201
+        return render_template('index.html', message='Hello World!')
+
+    if request.method == 'GET':
+        return render_template('signup.html')
 
 
 @auth_routes.route('/signin', methods=['POST'])

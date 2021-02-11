155e2543e00f1f1d38c2022d10a337a2bf5e079d
Jason Gerard <jasongerard321@gmail.com>
2021-02-11 17:41:24 -0500

    #35 refactor

--- app/controllers/post_controller.py
+++ app/controllers/post_controller.py
@@ -1,6 +1,7 @@
 from typing import Any
 from flask import request, Blueprint, render_template, redirect, url_for, flash
 from app.services import post_service
+from app.utils.file import FORM_POST_IMAGE
 from app.utils.session_decorators import session_required
 from app.utils.validation import is_post_image_valid
 
@@ -19,7 +20,7 @@ def create_post(user_id: int) -> Any:
             flash('A valid image is required to make a post')
             return redirect(request.url)
 
-        file = request.files['post_image']
+        file = request.files[FORM_POST_IMAGE]
 
         post = post_service.post_builder(request, user_id)
         post_service.create_post(post, file)

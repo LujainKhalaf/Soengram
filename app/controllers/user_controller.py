f55fbaa7cb924569386e45eeffa922812e909aae
fadi-albasha <62609433+fadi-albasha@users.noreply.github.com>
2021-02-13 11:30:02 -0500

    Update user_controller.py

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -9,10 +9,10 @@ user_routes = Blueprint('user_routes', __name__)
 
 @user_routes.route('/<username>', methods=['GET'])
 @session_required
-def get_user(username) -> Any:
+def get_user(user_id, username) -> Any:
     user = User.get_by_username(username)
     if user:
-        is_me = user.user_id == session['token']
+        is_me = user.user_id == user_id
         return render_template('profile.html', user=user, is_me=is_me), 200
     else:
         return '', 404

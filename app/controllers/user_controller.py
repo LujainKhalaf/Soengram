3fb9bf00be1d39eac0c54a2664c0b032d21fff4f
fadi-albasha <62609433+fadi-albasha@users.noreply.github.com>
2021-02-13 10:58:28 -0500

    Update user_controller.py

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,5 +1,5 @@
-from typing import Tuple
-from flask import Blueprint
+from typing import Any
+from flask import Blueprint, render_template, session
 from app.models import User
 from app.utils.session_decorators import session_required
 
@@ -9,9 +9,11 @@ user_routes = Blueprint('user_routes', __name__)
 
 @user_routes.route('/<username>', methods=['GET'])
 @session_required
-def get_user(username) -> Tuple[str, int]:
+def get_user(username) -> Any:
     user = User.get_by_username(username)
     if user:
-        return '', 200
+        is_me = user.user_id == session['token']
+        return render_template('profile.html', user=user, is_me=is_me), 200
     else:
         return '', 204
+    

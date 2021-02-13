76f479c40df70ea0c323ba63e684ddc034a55cb7
Fadi Albasha <fadi.albasha@gmail.com>
2021-02-13 13:44:11 -0500

    #55 Implement endpoint to delete post

--- app/controllers/post_controller.py
+++ app/controllers/post_controller.py
@@ -4,6 +4,7 @@ from app.services import post_service
 from app.utils.file import FORM_POST_IMAGE
 from app.utils.session_decorators import session_required
 from app.utils.validation import is_post_image_valid
+from app.models import Post
 
 post_routes = Blueprint('post_routes', __name__)
 
@@ -26,3 +27,16 @@ def create_post(user_id: int) -> Any:
         post_service.create_post(post, file)
 
         return redirect(url_for("index_routes.index"))
+
+
+@post_routes.route('/delete', methods=['POST'])
+@session_required
+def delete_post(user_id) -> Any:
+    post_id = request.form.values()
+    post = Post.get_by_post_id(post_id)
+    if post:
+        if post.user_id == user_id:
+            Post.delete(post)
+            return redirect(url_for("index_routes.index"))
+    return '', 404
+

31c94b7c15dd31fa0fa10ab2ca4f2701dce98ac2
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2021-02-19 21:32:40 -0500

    #108 Fixed delete post bug
    - Moved delete icon inside the modal header
    - Added jQuery support

--- app/static/posts.js
+++ app/static/posts.js
@@ -2,13 +2,20 @@ async function deletePost(e) {
     const postId = await e.getAttribute('data-post-id');
     const formData = new FormData();
     formData.append('post_id', postId);
-    await fetch('http://127.0.0.1:5000/delete', {
+
+    // Close Bootstrap modal
+    $('#post-modal-'+postId).modal('hide')
+
+    await fetch('/delete', {
         method: 'POST',
         body: formData
     });
     const gridItem = await document.getElementById(`post-grid-item-${postId}`);
     gridItem.parentNode.removeChild(gridItem);
-  
+
+    var modalItem = document.getElementById(`post-modal-${postId}`);
+    modalItem.parentNode.removeChild(modalItem)
+
     let postCount = parseInt(document.getElementById("profile-post-count").innerText);
     let updatedPostCount = postCount-1;
 

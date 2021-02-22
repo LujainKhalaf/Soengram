ed64e97d78ad8906751d5f6a8058b7fb11852a87
Mahmoud <m.nashatelsayed@gmail.com>
2021-02-21 20:50:29 -0500

    #67 code refactoring

--- app/static/posts.js
+++ app/static/posts.js
@@ -26,27 +26,3 @@ async function deletePost(e) {
         document.getElementById("upload-first").classList.remove("invisible")
     }
 }
-
-async function follow(e) {
-    const userId = await e.getAttribute('this-user-id');
-    const formData = new FormData();
-    formData.append('user_id', userId);
-
-    const followButton = document.getElementById('follow-button');
-    const isFollowing = followButton.innerText === 'Follow';
-    if (isFollowing) {
-        await fetch('/follow', {method: 'POST', body: formData});
-        followButton.innerText = 'Following';
-        followButton.title = 'Following';
-        followButton.className = 'btn btn-following';
-    } else {
-        await fetch('/unfollow', {method: 'POST', body: formData});
-        followButton.innerText = 'Follow';
-        followButton.title = 'Follow';
-        followButton.className = 'btn btn-primary';
-    }
-
-    const followerCount = document.getElementById('follow-button-follower-count');
-    const currentCount = parseInt(followerCount.innerText)
-    followerCount.innerText = isFollowing ? `${currentCount + 1}` : `${currentCount - 1}`;
-}

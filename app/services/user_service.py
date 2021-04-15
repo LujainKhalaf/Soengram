d4538e80b2c55b426415ecf1af5e545d276535a0
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2021-04-15 14:52:20 -0400

    #166 Clean up redundant code and refactor directory structure

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -47,7 +47,7 @@ def follow_user(user_id: int, user_id_to_follow: int) -> JSONResponse:
     User.add_to_following(user_id, user_id_to_follow)
     user = User.get_by_user_id(user_id)
     return (
-        render_template("components/following_modal/_following-item.html", user=user),
+        render_template("components/follow/_follow_item.html", user=user),
         200,
     )
 
